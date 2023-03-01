45b607cbd201279bfd35236e2d10917a6472244a
Marcin Chmurowski <sisimoski@outlook.com>
2023-03-01 22:18:08 +0100

    removed MainPage

--- Rad.io.Client.MAUI/MauiProgram.cs
+++ Rad.io.Client.MAUI/MauiProgram.cs
@@ -2,6 +2,7 @@
 using Rad.io.Client.MAUI.ViewModels;
 using Rad.io.Client.MAUI.Pages;
 using RadioBrowser.Net.Services;
+using CommunityToolkit.Maui;
 
 namespace Rad.io.Client.MAUI;
 
@@ -12,13 +13,13 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMauiCommunityToolkitMediaElement()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
         builder.Services.AddRadioBrowserServices("Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15");
-        builder.Services.AddTransient<MainPage>();
         builder.Services.AddTransient<NowPlayingPage>();
         builder.Services.AddTransient<NowPlayingViewModel>();
 
